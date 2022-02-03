d066b48aaff1319bd492ce3900f546de15091118
borisbrnkalak <borisbrnkalak@gmail.com>
2022-02-03 21:25:33 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <header class="main-header">
+    <div class="navigation-links">
+      <ul class="links">
+        <li class="specific-link features">
+          <div class="arrow-text">
+            <a href="#">Features</a>
+            <i class="fas fa-caret-down"></i>
+          </div>
+        </li>
+        <li class="specific-link pricing"><a href="#">Pricing</a></li>
+        <li class="specific-link contact">
+          <div class="arrow-text">
+            <a href="#">Contact</a>
+            <i class="fas fa-caret-down"></i>
+          </div>
+        </li>
+        <li class="specific-link learn">
+          <div class="arrow-text">
+            <a href="#">Learn</a>
+            <i class="fas fa-caret-down"></i>
+          </div>
+        </li>
+      </ul>
+    </div>
+
+    <div class="login-btns">
+      <div class="register">
+        <button class="register-btn">
+          <div class="content">
+            <span class="large">Register</span>
+            <span class="tiny">for free</span>
+          </div>
+        </button>
+      </div>
+
+      <div class="login">
+        <button class="login-btn"><span class="large">Log In</span></button>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Header",
+
+  components: {},
+
+  setup() {},
+});
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-page-container class="main-container bg-background-color">
+      <div class="container">
+        <Header />
+
+        <p class="bg-yellow">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, alias!
+          Tempora aspernatur dicta molestias porro dolorum veniam officiis
+          praesentium aut temporibus? Laudantium, laboriosam placeat dolorum at
+          est ipsum optio nihil!
+        </p>
+      </div>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import Header from "components/Header.vue";
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: { Header },
+
+  setup() {},
+});
+</script>
